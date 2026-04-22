87be9db39efb7b872723cde68e40629cc3847b2d
develper21 <githubnarvin@gmail.com>
2026-04-22 13:04:46 +0530

    server: add database module with connection provider

---
+++ server/src/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from './schema';
+
+export const DATABASE_CONNECTION = 'DATABASE_CONNECTION';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          user: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+        });
+        
+        return drizzle(pool, { schema });
+      },
+    },
+  ],
+  exports: [DATABASE_CONNECTION],
+})
+export class DatabaseModule {}
