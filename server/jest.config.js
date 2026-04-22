7cc31ad02535b74dd0e95a71125842c27e7cb0ba
develper21 <githubnarvin@gmail.com>
2026-04-22 13:22:32 +0530

    server: add Jest testing configuration

---
+++ server/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/*.module.ts',
+    '!**/main.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['../test/setup.ts'],
+  testTimeout: 10000,
+};
